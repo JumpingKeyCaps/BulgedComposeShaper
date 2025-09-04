d5966c9763c1286f51df1b0e0bf880d2cadd82ff
Samuel Lebailly <sam.meusa@gmail.com>
2025-09-04 20:41:24 +0200

    feature(composable/): Add documentation to the BulgedImage composable.

--- app/src/main/java/com/lebaillyapp/bulgedcomposeshaper/composable/BulgedImage3.kt
+++ app/src/main/java/com/lebaillyapp/bulgedcomposeshaper/composable/BulgedImage3.kt
@@ -1,7 +1,5 @@
 package com.lebaillyapp.bulgedcomposeshaper.composable
 
-import android.os.Build
-import androidx.annotation.RequiresApi
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.ImageBitmap
 import androidx.compose.foundation.Image
@@ -14,10 +12,36 @@ import androidx.compose.ui.graphics.painter.BitmapPainter
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.unit.dp
 import com.lebaillyapp.bulgedcomposeshaper.bulgedShape.BulgedRectangleSmoothShape
-import com.lebaillyapp.bulgedcomposeshaper.bulgedShape.old.BulgedRoundedRectangleShape
 import kotlin.math.min
 
-@RequiresApi(Build.VERSION_CODES.TIRAMISU)
+
+
+
+/**
+ * A [Composable] that displays an [ImageBitmap] clipped into a custom
+ * [Shape] with bulging edges.
+ *
+ * Internally, the image is automatically scaled up to ensure the bulge
+ * deformation does not reveal empty background at the edges.
+ * The zoom factor is computed based on the minimum image dimension and
+ * the shape's `bulgeAmount`.
+ *
+ * #### Parameters
+ * @param bitmap The [ImageBitmap] to display.
+ * @param contentDescription Description of the image for accessibility.
+ * @param shape The clipping [Shape].
+ * Defaults to a [BulgedRectangleSmoothShape] with:
+ * - corner radius = `10.dp`
+ * - bulge amount = `0.07f`
+ *
+ * #### Notes
+ * - Uses [ContentScale.Crop] to fill the available space.
+ * - Applies a dynamic [graphicsLayer] scaling to cover bulge displacement.
+ *
+ *
+ * @see BulgedRectangleSmoothShape
+ * @see androidx.compose.foundation.Image
+ */
 @Composable
 fun BulgedImage3(
     bitmap: ImageBitmap,
